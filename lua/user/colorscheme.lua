fb5cd0284a434984d2fb4292055ab4689c057a24
Daniel Ise <ise.daniel@gmail.com>
2022-10-21 18:40:28 -0300

    comments without italics

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,33 @@
+-- make protected calls
+local status_ok, lush = pcall(require, 'lush')
+if not status_ok then
+  return
+end
+
+local status_ok, darcula_solid = pcall(require, 'lush_theme.darcula-solid')
+if not status_ok then
+  return
+end
+
+-- define the customize specs
+local specs = lush.extends({darcula_solid}).with(function()
+  return {
+    -- use default values for comments, but change the gui to regular
+    Comment { fg = darcula_solid.Comment.fg, 
+              bg = darcula_solid.Comment.bg, 
+              gui = "regular"
+    },
+  }
+end)
+
+-- define a customise function
+_G.customise_colorscheme = function()
+  lush(specs)
+end
+
+-- use an autocommand that changes the colorscheme on VimEnter
+vim.cmd[[autocmd VimEnter,ColorScheme * lua customise_colorscheme()]]
+
+-- set the colorscheme
+vim.cmd[[colorscheme darcula-solid]]
+
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -7,7 +7,6 @@ local term_opts = { silent = true }
 --keymaps
 g.mapleader = ' '                       --<space> as mapleader
 keymap.set('n', 'x', '"_x')             --x sin yank
-keymap.set('n', '<C-a>', 'gg<S-v>G')    --selectall
 keymap.set('n', '<Leader>e', ':Ex<CR>') --<space>e abre netrw
 keymap.set('n', '<bs>', ':nohlsearch<cr>', { silent = true }) --sacar hl con backspace
 
